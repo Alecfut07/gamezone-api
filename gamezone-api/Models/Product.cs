b613732c3caef174dd24530d7a959f7ab4698617
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2023-04-17 12:44:14 -0600

    Initial commit

---
+++ gamezone-api/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace gamezone_api.Models
+{
+	public class Product
+	{
+        [Required]
+        public Guid ProductId { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public decimal Price { get; set; }
+
+        public DateOnly ReleaseDate { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime CreateDate { get; set; }
+
+        [Required]
+        public DateTime UpdateDate { get; set; }
+    }
+}
+
---
+++ gamezone-api/Program.cs
@@ -0,0 +1,32 @@
+﻿using gamezone_api;
+using gamezone_api.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// SQL SERVER CONNECTION
+//builder.Services.AddSqlServer<Product>(builder.Configuration.GetConnectionString("SQL_Server"));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
