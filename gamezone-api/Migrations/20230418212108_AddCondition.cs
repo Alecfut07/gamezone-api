1e103b56ee776d2dba6b83059432f3f540d74a03
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2023-04-18 15:54:43 -0600

    Adding new Migration (AddCondition) and adding new Model (Condition)

---
+++ gamezone-api/Migrations/20230418212108_AddCondition.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace gamezone_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCondition : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "conditions",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    state = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false),
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_conditions", x => x.id);
+                });
+
+            //migrationBuilder.InsertData(
+            //    table: "conditions",
+            //    columns: new[] { "id", "state" },
+            //    values: new object[,]
+            //    {
+            //        { 1, "NEW" },
+            //        { 2, "PRE_OWNED" },
+            //        { 3, "DIGITAL" },
+            //    });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "conditions");
+        }
+    }
+}
---
+++ gamezone-api/Models/Condition.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace gamezone_api.Models
+{
+	public class Condition
+	{
+		[Key]
+		[Column("id")]
+		public int Id { get; set; }
+
+		[Required]
+		[Column("state")]
+		public string State { get; set; }
+	}
+}
+
