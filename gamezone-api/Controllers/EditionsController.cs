5b23f8180d961aec9c04c023668e52602cf60b0b
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2023-04-20 08:52:42 -0600

    CRUD of Editions is DONE

--- gamezone-api/Controllers/EditionsController.cs
+++ gamezone-api/Controllers/EditionsController.cs
@@ -5,32 +5,85 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace gamezone_api.Controllers
 {
-	[ApiController]
-	[Route("[controller]")]
-	public class EditionsController : ControllerBase
-	{
-		IEditionService editionService;
+    [ApiController]
+    [Route("[controller]")]
+    public class EditionsController : ControllerBase
+    {
+        IEditionService editionService;
 
-		public EditionsController(IEditionService editionService)
-		{
-			this.editionService = editionService;
-		}
+        public EditionsController(IEditionService editionService)
+        {
+            this.editionService = editionService;
+        }
 
-		// GET: /editions
-		[HttpGet]
-		public async Task<ActionResult<Edition?>> GetEditions()
-		{
-			var editions = await editionService.GetEditions();
-			return Ok(editions);
-		}
+        // GET: /editions
+        [HttpGet]
+        public async Task<ActionResult<Edition?>> GetEditions()
+        {
+            var editions = await editionService.GetEditions();
+            return Ok(editions);
+        }
 
-		// GET by id: /editions/id
-		[HttpGet("{id}")]
-		public async Task<ActionResult<Edition?>> GetEditionById(int id)
-		{
-			var edtion = await editionService.GetEditionById(id);
-			return edtion;
-		}
-	}
+        // GET by id: /editions/id
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Edition?>> GetEditionById([FromRoute] int id)
+        {
+            var edition = await editionService.GetEditionById(id);
+            if (edition == null)
+            {
+                return NotFound();
+            }
+            return Ok(edition);
+        }
+
+        // POST: /editions
+        [HttpPost]
+        public async Task<ActionResult<Edition?>> CreateNewEdition([FromBody] Edition edition)
+        {
+            if (!ModelState.IsValid)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            else
+            {
+                var newEdition = await editionService.CreateNewEdition(edition);
+                return Ok(newEdition);
+            }
+        }
+
+        // PUT: /editions/id
+        [HttpPut("{id}")]
+        public async Task<ActionResult<Edition?>> UpdateEdition([FromRoute] int id, [FromBody] Edition edition)
+        {
+            try
+            {
+                var updatedEdition = await editionService.UpdateEdition(id, edition);
+                return Ok(updatedEdition);
+            }
+            catch (Exception ex)
+            {
+                return NotFound();
+            }
+        }
+
+        // DELETE: /editions/id
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Edition?>> DeleteEdition([FromRoute] int id)
+        {
+            try
+            {
+                await editionService.DeleteEdition(id);
+            }
+            catch(Microsoft.EntityFrameworkCore.DbUpdateException ex)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+            catch (ArgumentException ex)
+            {
+                return NotFound();
+            }
+            return NoContent();
+        }
+    }
 }
 
--- gamezone-api/Services/EditionService.cs
+++ gamezone-api/Services/EditionService.cs
@@ -24,6 +24,45 @@ namespace gamezone_api.Services
 			var edition = await context.Editions.FindAsync(id);
 			return edition;
 		}
+
+		public async Task<Edition?> CreateNewEdition(Edition newEdition)
+		{
+			context.Editions.Add(newEdition);
+			await context.SaveChangesAsync();
+			return newEdition;
+		}
+
+		public async Task<Edition?> UpdateEdition(int id, Edition edition)
+		{
+			var editionToUpdate = await context.Editions.FindAsync(id);
+
+			if (editionToUpdate == null)
+			{
+				throw new ArgumentException();
+			}
+			else
+			{
+				editionToUpdate.Type = edition.Type;
+				await context.SaveChangesAsync();
+			}
+
+			return editionToUpdate;
+		}
+
+		public async Task DeleteEdition(int id)
+		{
+			var editionToDelete = await context.Editions.FindAsync(id);
+
+			if (editionToDelete == null)
+			{
+				throw new ArgumentException();
+			}
+			else
+			{
+				context.Editions.Remove(editionToDelete);
+				await context.SaveChangesAsync();
+			}
+		}
 	}
 
 	public interface IEditionService
@@ -31,6 +70,12 @@ namespace gamezone_api.Services
 		Task<IEnumerable<Edition?>> GetEditions();
 
 		Task<Edition?> GetEditionById(int id);
+
+		Task<Edition?> CreateNewEdition(Edition newEdition);
+
+		Task<Edition?> UpdateEdition(int id, Edition edition);
+
+		Task DeleteEdition(int id);
 	}
 }
 
