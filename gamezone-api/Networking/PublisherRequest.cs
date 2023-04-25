c7e83892e3079c6d6bf54d3d34f3ea153de99aed
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2023-04-25 17:14:59 -0600

    Adding Publisher Request and Response files

---
+++ gamezone-api/Networking/PublisherRequest.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace gamezone_api.Networking
+{
+    public class PublisherRequest
+	{
+		[Required]
+		public string Name { get; set; }
+	}
+}
+
