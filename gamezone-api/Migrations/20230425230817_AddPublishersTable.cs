bdf0a3ea919ccb3865484c228c22ac3fb89309d1
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2023-04-25 17:12:13 -0600

    Adding new Migration: AddPublishersTable

---
+++ gamezone-api/Migrations/20230425230817_AddPublishersTable.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace gamezone_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPublishersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "publishers",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    name = table.Column<string>(type: "nvarchar(30)", maxLength: 30, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_publishers", x => x.id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "publishers",
+                columns: new[] { "id", "name" },
+                values: new object[,]
+                {
+                    { 1, "Nintendo" },
+                    { 2, "Sony" },
+                    { 3, "Microsoft" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "publishers");
+        }
+    }
+}
