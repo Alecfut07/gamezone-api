4f916e52ea9279bfad717a23b29b622c27952eea
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2023-06-04 12:22:11 -0600

    Add new migration: RemoveUniqueConstraintHandleToCategoriesTable

--- gamezone-api/GamezoneContext.cs
+++ gamezone-api/GamezoneContext.cs
@@ -61,8 +61,6 @@ namespace gamezone_api
 
                 category.Property(c => c.Handle).IsRequired().HasMaxLength(40);
 
-                category.HasIndex(c => c.Handle).IsUnique();
-
                 category.Property(c => c.CreateDate).IsRequired();
 
                 category.Property(c => c.UpdateDate).IsRequired();
---
+++ gamezone-api/Migrations/20230604152905_RemoveUniqueConstraintHandleToCategoriesTable.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace gamezone_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveUniqueConstraintHandleToCategoriesTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_categories_handle",
+                table: "categories");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_categories_handle",
+                table: "categories",
+                column: "handle",
+                unique: true);
+        }
+    }
+}
