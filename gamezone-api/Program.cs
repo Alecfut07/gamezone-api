cdb7bbe08be3e6a0a58b777da1a589f2cad88e85
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2023-06-11 17:32:01 -0600

    Add new origin to policy rules

--- gamezone-api/Program.cs
+++ gamezone-api/Program.cs
@@ -39,7 +39,7 @@ builder.Services.AddCors(options =>
                       policy =>
                       {
                           //policy.WithOrigins("http://localhost:3000")
-                          policy.WithOrigins("http://localhost:3000", "http://web:80", "http://web:3000")
+                          policy.WithOrigins("http://localhost", "http://localhost:3000", "http://web", "http://web:3000")
                               .AllowAnyHeader()
                               .AllowAnyMethod()
                               //.AllowAnyOrigin()
