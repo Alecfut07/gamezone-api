290a912eba6c7adbf24c64f30d62c40b57fcb7a8
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2023-06-12 18:25:35 -0600

    New Project: gamezone-migration

---
+++ gamezone-migration/Program.cs
@@ -0,0 +1,16 @@
+﻿using gamezone_api;
+using gamezone_migration;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+
+DotNetEnv.Env.Load();
+Console.WriteLine("Applying migrations");
+var webHost = new WebHostBuilder()
+    .UseContentRoot(Directory.GetCurrentDirectory())
+    .UseStartup<ConsoleStartup>()
+    .Build();
+using (var context = (GamezoneContext)webHost.Services.GetService(typeof(GamezoneContext)))
+{
+    context.Database.Migrate();
+}
+Console.WriteLine("Done");
