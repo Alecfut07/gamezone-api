4fabd1d9e9813b56a379ba014dd681158caad733
Alec Ortega Cisneros <alec.ortega.cisneros@gmail.com>
2023-07-08 15:41:32 -0600

    Add override Equals method in Edition and EditionResponse

--- gamezone-api/Models/Edition.cs
+++ gamezone-api/Models/Edition.cs
@@ -12,6 +12,17 @@ namespace gamezone_api.Models
 		[Required]
 		[Column("type")]
 		public string Type { get; set; }
-	}
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) { return false; }
+            if (object.ReferenceEquals(this, obj)) { return true; }
+
+            var edition = obj as Edition;
+            return
+                this.Id == edition.Id &&
+                this.Type == edition.Type;
+        }
+    }
 }
 
--- gamezone-api/Networking/EditionResponse.cs
+++ gamezone-api/Networking/EditionResponse.cs
@@ -1,10 +1,23 @@
-﻿namespace gamezone_api.Networking
+﻿using gamezone_api.Models;
+
+namespace gamezone_api.Networking
 {
     public class EditionResponse
 	{
 		public int Id { get; set; }
 
 		public string Type { get; set; }
-	}
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) { return false; }
+            if (object.ReferenceEquals(this, obj)) { return true; }
+
+            var editionResponse = obj as EditionResponse;
+            return
+                this.Id == editionResponse.Id &&
+                this.Type == editionResponse.Type;
+        }
+    }
 }
 
